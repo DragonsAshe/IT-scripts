9f02453cdd58a93ad33c7b2f42a4fa6fd73ad663
Ashley <solidgitmail@gmail.com>
2024-10-24 14:09:41 +0200

    Added Intune Hash and WCUser scripts

---
+++ Windows 10 Scripts/Intune Hash/IntuneHash.bat
@@ -0,0 +1,4 @@
+@echo off
+color D
+Powershell.exe -ExecutionPolicy Bypass -File IntuneHash.ps1
+echo Ashley was here :3
---
+++ Windows 10 Scripts/Intune Hash/IntuneHash.ps1
@@ -0,0 +1,4 @@
+﻿Set-Executionpolicy Bypass -Force
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Confirm:$False
+Install-Script -Name Get-WindowsAutoPilotInfo -Force -Confirm:$False
+powershell.exe -ExecutionPolicy Bypass Get-WindowsAutopilotInfo.ps1 -Online -Assign
